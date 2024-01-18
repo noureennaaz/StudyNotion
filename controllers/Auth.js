ec2c51aa7ce5dcd2c7a05b1536abc6e965dbc7d8
noureennaaz <noureennaaz09@gmail.com>
2024-01-18 19:16:47 +0530

    login and otp-generator controller added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,6 +3,48 @@ const User=require('../models/Users')
 const OTPModel=require("../models/OTPModel")
 const ProfileInfo=require('../models/ProfileInfo')
 const bcrypt=require('bcrypt')
+const otpgenerator = require('otp-generator')
+const jwt =require('jsonwebtoken')
+require('dotenv').config()
+
+exports.sendOTP = async ( req, res ) =>{
+    try{
+        const {email} = request.body
+    const existing = await OTPModel.findOne({email})
+
+    if(existing){
+        return res.status(400).json({
+            success:false,
+            message:"User already exists"
+        })
+    }
+
+    var otp = otpgenerator.generate( 4 , { upperCaseAlphabets: false , specialChars: false})
+    const findSameOtp=await OTPModel.findOne({otp});
+    while(findSameOtp){
+        otp = otpgenerator.generate( 4 , { upperCaseAlphabets: false , specialChars: false})
+        findSameOtp=await OTPModel.findOne({otp});
+    }
+
+    const OTPdata= new OTPModel.create({
+        email,
+        otp,
+        createdAt
+    })
+
+    return res.status(200).json({
+        success:true,
+        message:"OTP Generated"
+    })
+    } catch ( err ){
+        console.error (err)
+        return res.status(200).json({
+            success:false,
+            message:"OTP cannot be generated, try again"
+        })
+
+    }
+}
 exports.signup= async ( req, res ) => {
 
     try {
@@ -77,6 +119,59 @@ exports.signup= async ( req, res ) => {
         })
 
     }
+}
 
+exports.login= async (req, res) =>{
 
+    try {
+
+        const {email , password } =request.body;
+
+    if(!email || !password ){
+        return res.status(400).json({
+            success:false,
+            message: "Please fill all the details"
+        })
+    }
+
+    const user= await User.findOne({email})
+
+    if(!user){
+        return res.status(400).json({
+            success:false,
+            message: "User not found, try signing in first"
+        })
+    }
+
+    if(!bcrypt.compare(password, user.password )){
+        return res.status(400).json({
+            success:false,
+            message: "Wrong Password"
+        })
+    }
+     const payload={
+        email :user.email , 
+        role:user.accountType,
+        id : user._id
+     }
+    var token=await jwt.sign( payload , process.env.JWT_SECRET, {expiresIn :"2h"});
+    token= toObject(token)
+    user.token=token
+    user.password=undefined
+    
+
+    res.cookie("token", token , { expires: 1000* 3* 24 * 60 *60  , httpOnly:true }).status(200).json({
+        success:true, 
+        message:"Successfully logged in"  ,
+        token , 
+        user
+    })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({
+            success:false, 
+            message :'Login Failed, try again'
+        })
+    }
 }
